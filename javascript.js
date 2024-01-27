f836c9883a7d725d973205d8d430e942ea638498
TakenpaUser <100627977+TakenpaUser@users.noreply.github.com>
2024-01-27 18:12:59 +0300

    Update javascript.js
    Added debug

--- javascript.js
+++ javascript.js
@@ -1,2 +1,13 @@
 console.log("Javascript has been runned");
 alert("Script has been loaded");
+
+// For code debug
+
+// console.log("Step 1");
+
+// console.log("Step 2");
+
+// console.log("Step 3");
+
+// console.log("Step 4");
+
